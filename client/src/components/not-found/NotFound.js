cc05f5bad926392917604983f7249c9462278deb
kanghadong <hdkang2007@gmail.com>
2019-03-24 14:29:47 +0900

    first commit

---
+++ client/src/components/not-found/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div style={{minHeight: '100vh'}}>
+      <h1 className="display-4">Page Not Found</h1>
+      <p>Sorry, this page does not exist</p>
+    </div>
+  );
+};
+
+export default NotFound;
